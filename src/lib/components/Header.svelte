a629f35cc940efbe59979b43cde8ccce62b5c77b
Woodsii <beanmanwoods@gmail.com>
2025-07-04 17:16:05 -0400

    add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,33 @@
+<header>
+	<a href="/">Secret Handshake RnD</a>
+
+	<nav>
+		<ul>
+            <li><a href="/blog"> blog </a></li>
+            <li><a href="/portfolio"> portfolio </a></li>
+            <li><a href="/about-me"> about-me </a></li>
+        </ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		padding: 1rem;
+		background: lightskyblue;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+	}
+
+	ul {
+		margin: 0;
+		list-style-type: none;
+		display: flex;
+		gap: 1rem;
+	}
+
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+</style>
