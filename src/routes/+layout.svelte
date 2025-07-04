9fb29fb63115812f31cc880e06a8ac37a2c7c1a6
Woodsii <beanmanwoods@gmail.com>
2025-07-04 17:17:44 -0400

    impliment markdown blog

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+
+<script>
+import Header from '$lib/components/Header.svelte';
+import '$lib/styles/style.css';
+</script>
+
+<Header />
+
+<main>
+	<slot></slot>
+</main>
+
+<footer>foooooterrrrr</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,1 @@
-<h1> Secret Handshake R&D</h1>
-
-<p> Hey this is under construction! </p>
+<h1>LAWL</h1>
