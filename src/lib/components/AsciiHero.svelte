0023d17fd14145adde3067b4a059013e5d5a7e1a
Woodsii <beanmanwoods@gmail.com>
2025-07-19 18:00:34 -0400

    add threlte hero

---
+++ src/lib/components/AsciiHero.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scene from './AsciiScene.svelte'
+</script>
+
+<div class="hero">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  .hero {
+    width:100%;
+    height: 100vh
+  }
+</style>
---
+++ src/lib/components/AsciiScene.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { T, useTask } from '@threlte/core'
+  import { interactivity, AsciiRenderer } from '@threlte/extras'
+  import { Spring } from 'svelte/motion'
+
+  interactivity()
+  const scale = new Spring(1)
+
+  let rotation = $state(0)
+  useTask((delta) => {
+    rotation += delta
+  })
+</script>
+
+<T.DirectionalLight 
+    position={[0, 10, 10]} 
+    castShadow
+/>
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[5, 1, 5]}
+  oncreate={(ref) => {
+    ref.lookAt(0, 1, 0)
+  }}
+/>
+
+<T.Mesh 
+    rotation.y={rotation}
+    position.y={1}
+    scale={scale.current}
+    onpointerenter={() => {
+        scale.target = 1.5
+    }}
+    onpointerleave={() => {
+        scale.target = 1
+    }}
+    castShadow
+>
+    <T.BoxGeometry args={[1, 2, 1]} />
+    <T.MeshStandardMaterial color="#d8b4f8" />
+</T.Mesh>
+
+<T.Mesh
+  rotation.x={-Math.PI / 2}
+  receiveShadow
+>
+  <T.CircleGeometry args={[4, 40]} />
+  <T.MeshStandardMaterial color="white" />
+</T.Mesh>
+
+<style>
+  
+</style>
