340a485ac797d43954de57eefdc5970b7c2fee08
Woodsii <beanmanwoods@gmail.com>
2025-07-31 14:43:07 -0500

    add ascii renderer

--- src/lib/components/AsciiScene.svelte
+++ src/lib/components/AsciiScene.svelte
@@ -12,43 +12,45 @@
   })
 </script>
 
-<T.DirectionalLight 
-    position={[0, 10, 10]} 
-    castShadow
-/>
+<AsciiRenderer fgColor="#d8b4f8" bgColor="#000000" characters=" .:-+*=%@#" onstart={() => console.log('Started!')}>
+  <T.Mesh 
+        rotation.y={rotation}
+        position.y={1}
+        scale={scale.current}
+        onpointerenter={() => {
+            scale.target = 1.5
+        }}
+        onpointerleave={() => {
+            scale.target = 1
+        }}
+        castShadow
+    >
+        <T.BoxGeometry args={[1, 2, 1]} />
+        <T.MeshStandardMaterial color="#d8b4f8" />
+    </T.Mesh>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[5, 1, 5]}
-  oncreate={(ref) => {
-    ref.lookAt(0, 1, 0)
-  }}
-/>
+    <T.Mesh
+      rotation.x={-Math.PI / 2}
+      receiveShadow
+    >
+      <T.CircleGeometry args={[4, 40]} />
+      <T.MeshStandardMaterial color="white" />
+    </T.Mesh>
+</AsciiRenderer>
 
-<T.Mesh 
-    rotation.y={rotation}
-    position.y={1}
-    scale={scale.current}
-    onpointerenter={() => {
-        scale.target = 1.5
+<T.AmbientLight intensity={0.5} />
+
+ <T.DirectionalLight 
+      position={[0, 10, 10]} 
+      castShadow
+  />
+
+  <T.PerspectiveCamera
+    makeDefault
+    position={[5, 1, 5]}
+    oncreate={(ref) => {
+      ref.lookAt(0, 1, 0)
     }}
-    onpointerleave={() => {
-        scale.target = 1
-    }}
-    castShadow
->
-    <T.BoxGeometry args={[1, 2, 1]} />
-    <T.MeshStandardMaterial color="#d8b4f8" />
-</T.Mesh>
+  />
 
-<T.Mesh
-  rotation.x={-Math.PI / 2}
-  receiveShadow
->
-  <T.CircleGeometry args={[4, 40]} />
-  <T.MeshStandardMaterial color="white" />
-</T.Mesh>
 
-<style>
-  
-</style>
